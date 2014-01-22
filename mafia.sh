442ea4aed529ce4a4681bb03124ef86eea80994f
petRUShka <petrushkin@yandex.ru>
2014-01-22 09:45:06 +0400

    Добавили 13 и 14 игру, Переключились на 2014 год

--- mafia.sh
+++ mafia.sh
@@ -3,7 +3,7 @@ myvar=`cat`
 
 echo "    <li class="b-games-li">
       <h2>${1}-ая игра в «Убежище»</h2>
-      <p class=\"b-games-li-date\"> ${2} 2013 года</p>
+      <p class=\"b-games-li-date\"> ${2} 2014 года</p>
       <ul class=\"b-games-li-players\">"
 
 echo "$myvar" | sed 's/\(\w\+\s\w\+\)\s\+\(\w\+\)/        <li class="b-games-li-player">\1 <p class="b-games-li-points">\2<\/p><\/li>/g' |  sed 's/\(\w\+\s\w\+\)\s*$/        <li class="b-games-li-player">\1<\/li>/g'
