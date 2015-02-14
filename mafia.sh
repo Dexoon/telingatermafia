d5599615b0abcf7d9578b53d64bf57dfd3325c8f
petRUShka <petrushkin@yandex.ru>
2015-02-14 21:58:42 +0300

    Добавил 2-ую игру

--- mafia.sh
+++ mafia.sh
@@ -2,8 +2,8 @@
 myvar=`cat`
 
 echo "    <li class="b-games-li">
-      <h2>${1}-ая игра в «Кофе Хаузе»</h2>
-      <p class=\"b-games-li-date\"> ${2} 2014 года</p>
+      <h2>${1}-ая игра в «Кругозоре»</h2>
+      <p class=\"b-games-li-date\"> ${2} 2015 года</p>
       <ul class=\"b-games-li-players\">"
 
 echo "$myvar" | sed 's/\(\w\+\s\w\+\)\s\+\(\w\+\)/        <li class="b-games-li-player">\1 <p class="b-games-li-points">\2<\/p><\/li>/g' |  sed 's/\(\w\+\s\w\+\)\s*$/        <li class="b-games-li-player">\1<\/li>/g'
