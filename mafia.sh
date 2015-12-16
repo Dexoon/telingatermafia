673bae004d1db6ae6107d347b12fc54c9ecc345e
petRUShka <petrushkin@yandex.ru>
2015-12-16 13:01:44 +0300

    Исправил минус и Лови

--- mafia.sh
+++ mafia.sh
@@ -6,7 +6,7 @@ echo "    <li class="b-games-li">
       <p class=\"b-games-li-date\"> ${2} 2015 года</p>
       <ul class=\"b-games-li-players\">"
 
-      echo "$myvar" | sed 's/\s*\(.\+\)\s\+\([0-9]\+\)\s*/        <li class="b-games-li-player">\1 <p class="b-games-li-points">\2<\/p><\/li>/g' |  sed 's/^\s*\(\(\w\+\s*\)\+\)\s*$/        <li class="b-games-li-player">\1<\/li>/g'
+      echo "$myvar" | sed 's/\s*\(.\+\)\s\+\(-\?[0-9]\+\)\s*/        <li class="b-games-li-player">\1 <p class="b-games-li-points">\2<\/p><\/li>/g' |  sed 's/^\s*\(\(\w\+\s*\)\+\)\s*$/        <li class="b-games-li-player">\1<\/li>/g'
 
 echo "      </ul>
     </li>"
